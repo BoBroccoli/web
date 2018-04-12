05d74f2609a0fcae1d86f55d606197895f5a0aa9
Bo <wenxx194@d.umn.edu>
2018-04-12 11:14:46 -0500

    Directory

--- beerV1/src/com/example/model/BeerExpert.java
+++ beerV1/src/com/example/model/BeerExpert.java
@@ -1,5 +1,17 @@
 package com.example.model;
 
-public class BeerExpert {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BeerExpert {
+	public List<String> getBrands (String color){
+		List<String> brands = new ArrayList<>();
+		if (color.equals("amber")) {
+			brands.add("Jack Amber");
+			brands.add("Qingdao");
+		}
+		else
+			brands.add("Jail");
+		return brands;
+	}
 }
--- beerV1/src/com/example/web/BeerSelect.java
+++ beerV1/src/com/example/web/BeerSelect.java
@@ -1,5 +1,40 @@
 package com.example.web;
 
-public class BeerSelect {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.model.BeerExpert;
+import com.example.model.Person;
+
+public class BeerSelect extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5212877687087164982L;
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//resp.setContentType("text/html");
+		//PrintWriter pWriter = resp.getWriter();
+		//pWriter.println("<html>" + "<body>Beer selection advice<br>");
+		String c = req.getParameter("color");
+		BeerExpert beerExpert = new BeerExpert();
+		List<String> result = beerExpert.getBrands(c);
+		/*
+		 * for (String string : result) { pWriter.println("<br> try "+string); }
+		 * pWriter.println("<br>Got beer color "+c+"</body></html>");
+		 */
+		req.setAttribute("styles", result);
+		RequestDispatcher view = req.getRequestDispatcher("/result.jsp");
+		Person person = new Person("Bo");
+		req.setAttribute("person", person);
+		view.forward(req, resp);
+	}
 }
